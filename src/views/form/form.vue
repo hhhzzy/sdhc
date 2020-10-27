463a922907719dcfd2025e55262996b4c524257a
hzy <1193457593@qq.com>
2020-10-27 18:44:14 +0800

    提交

---
+++ src/views/form/form.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="form-box">
+
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        mineType: {
+            type: String,
+            default: 'ck'
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+        console.log(this.mineType)
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/views/list/list.vue
+++ src/views/list/list.vue
@@ -1,17 +1,32 @@
 <template>
     <div class="list-box">
-        <div class="btn-box"></div>
-        <Tabs type="card" :animated="false">
-            <TabPane label="探矿权人公示信息核查">标签一的内容</TabPane>
-            <TabPane label="采矿权人公示信息核查">标签二的内容</TabPane>
-            <TabPane label="矿山年底实地核查">标签三的内容</TabPane>
+        <div class="btn-box">
+
+        </div>
+        <Tabs type="card" :animated="false" @on-click="ClkTab">
+            <TabPane name="tk" label="探矿权人公示信息核查">标签一的内容</TabPane>
+            <TabPane name="ck" label="采矿权人公示信息核查">标签二的内容</TabPane>
+            <TabPane name="ks" label="矿山年底实地核查">标签三的内容</TabPane>
         </Tabs>
+        <Form type="mineType" />
     </div>
 </template>
 <script>
+import Form from '../form/form'
 export default {
+    components: {
+        Form
+    },
     data() {
-        return {}
+        return {
+            mineType: 'tk'
+        }
+    },
+    methods: {
+        ClkTab(name) {
+            console.log(name)
+            this.mineType = name
+        }
     }
 }
 </script>
